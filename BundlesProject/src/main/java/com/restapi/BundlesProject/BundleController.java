7c5ffc436483e61d12239bcdac66b523747b3b6e
Christos Daoulas <daoulas@outlook.com>
2020-07-19 20:09:00 +0300

    Update order functions

--- BundlesProject/src/main/java/com/restapi/BundlesProject/BundleController.java
+++ BundlesProject/src/main/java/com/restapi/BundlesProject/BundleController.java
@@ -5,26 +5,37 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.sun.istack.NotNull;
 
 @RestController
 class BundlesController {
 
   private final BundleRepository repository;
-
+  
+  @Autowired
   BundlesController(BundleRepository repository) {
     this.repository = repository;
     
+  
     //CHECK EVERY 30 SECONDS FOR EXPIRED BUNDLES AND DEACTIVATE THEM
 	Timer timer = new Timer ();
 	TimerTask t = new TimerTask () {
@@ -80,64 +91,55 @@ class BundlesController {
   
   //ADD NEW BUNDLE
   @PostMapping(path = "/bundles", consumes = "application/json")
-  public String addMember(@RequestBody Bundle bundle) {
+  public void addMember(@Validated @RequestBody Bundle bundle) throws ResponseStatusException{
 	  Bundle tempBundle= repository.findById(bundle.getCode())
 			  							.orElse(null);
 	  Boolean alreadyExists = tempBundle!=null;
-      String message;
-      if(bundle.getName().isEmpty())
-    	  message="Error!";			  
-      else
-    	  if(alreadyExists)
-    		  message = "Bundle already exists. Please use PUT if you want to update it.";
-		  else {
-			  message="Bundle was succesfully added.";
-			  repository.save(bundle);
-		  }
-      return message;
+
+	  if(alreadyExists)
+		  throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bundle already exists. Please use PUT if you want to update it.");
+	  else {
+		  repository.save(bundle);
+		  throw new ResponseStatusException(HttpStatus.OK, "Bundle was succesfully added.");  
+	  }
+	  
   }
   
   //UPDATE A BUNDLE
-  @PutMapping(path = "/bundles/{code}", consumes = "application/json", produces = "application/json")
-  public String updateMember(@RequestBody Bundle bundle) {
-	  Bundle tempBundle= repository.findById(bundle.getCode())
+  @PutMapping(path = "/bundles/{code}", consumes = "application/json")
+  public void updateMember(@Validated @RequestBody Bundle bundle, @PathVariable Long code) throws ResponseStatusException{
+	  Bundle tempBundle= repository.findById(code)
 			  							.orElse(null);
 	  Boolean alreadyExists = tempBundle!=null;
-      String message;
-      if(!bundle.getName().isEmpty())
-    	  if(alreadyExists) {
-    		  message = "Bundle was succesfully updated.";
-    		  repository.save(bundle);
-    	  }
-		  else 
-			  message="Bundle doesn't exist.";		  
-      else
-    	  message="Error!";
-      return message;
+	  if(alreadyExists) {
+		  repository.save(bundle);
+		  throw new ResponseStatusException(HttpStatus.OK, "Bundle was succesfully updated.");
+	  }else 
+		  throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bundle doesn't exist");	  
+	  
   }
   
   //DELETE A BUNDLE
   @DeleteMapping(path = "/bundles/{code}")
-  public String deleteMember(@PathVariable Long code) {
+  public void deleteMember(@PathVariable Long code) throws ResponseStatusException {
 	  Bundle tempBundle= repository.findById(code)
 			  						.orElse(null);
 	  Boolean alreadyExists = tempBundle!=null;
-      String message;
 	  if(alreadyExists) {
-		  message = "Bundle was succesfully deleted.";
 		  repository.deleteById(code);
+		  throw new ResponseStatusException(HttpStatus.OK, "Bundle was succesfully deleted.");
 	  }
 	  else 
-		  message="Bundle doesn't exist.";		  
+		  throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Bundle doesn't exist");			  
 
-      return message;
   }
   
-  //ORDER BUNDLES ASCENDING BY NAME
-  @GetMapping(value="/bundles/sortbyname", produces = MediaType.APPLICATION_JSON_VALUE)
-  ResponseEntity<List<Bundle>> ascendingbyname() {
+  //ORDER BUNDLES
+  @GetMapping(value="/bundles/sort", produces = MediaType.APPLICATION_JSON_VALUE)
+  ResponseEntity<List<Bundle>> ascendingbyname(@Param("by") String by, @Param("dir") String dir) {
+	 
 	  List<Bundle> bundles = new ArrayList<>();
-	  for(Bundle bundle : repository.findAll(sortByNameAsc())) {
+	  for(Bundle bundle : repository.findAll(sortBy(by,dir))) {
 	        bundles.add(bundle);
 	    }
 	  ResponseEntity<List<Bundle>> response;
@@ -148,48 +150,15 @@ class BundlesController {
 	  
 	  return response;
   }
-  private Sort sortByNameAsc() {
-	 Sort sort = Sort.by(Sort.Order.asc("name"));
-	 return sort;
+  private Sort sortBy(String by, String dir) {
+	  Sort sort = null;
+	  if(dir.equals("asc")) {
+		  sort = Sort.by(Sort.Order.asc(by));	  
+	  }else if((dir.equals("desc"))){
+		  sort = Sort.by(Sort.Order.desc(by));
+	  }
+	return sort;	 
   }
   
-  //ORDER BUNDLES ASCENDING BY PRICE
-  @GetMapping(value="/bundles/sortbypriceasc", produces = MediaType.APPLICATION_JSON_VALUE)
-  ResponseEntity<List<Bundle>> ascendingbyprice() {
-	  List<Bundle> bundles = new ArrayList<>();
-	  for(Bundle bundle : repository.findAll(sortByPriceAsc())) {
-	        bundles.add(bundle);
-	    }
-	  ResponseEntity<List<Bundle>> response;
-	  if(!bundles.isEmpty())
-		  response = new ResponseEntity<List<Bundle>>(bundles, HttpStatus.OK);
-	  else
-		  response = new ResponseEntity<List<Bundle>>(HttpStatus.NOT_FOUND);
-	  
-	  return response;
-  }
-  private Sort sortByPriceAsc() {
-	 Sort sort = Sort.by(Sort.Order.asc("price"));
-	 return sort;
-  }
   
-//ORDER BUNDLES DESCENDING BY PRICE
-  @GetMapping(value="/bundles/sortbypricedesc", produces = MediaType.APPLICATION_JSON_VALUE)
-  ResponseEntity<List<Bundle>> descendingbyprice() {
-	  List<Bundle> bundles = new ArrayList<>();
-	  for(Bundle bundle : repository.findAll(sortByPriceDesc())) {
-	        bundles.add(bundle);
-	    }
-	  ResponseEntity<List<Bundle>> response;
-	  if(!bundles.isEmpty())
-		  response = new ResponseEntity<List<Bundle>>(bundles, HttpStatus.OK);
-	  else
-		  response = new ResponseEntity<List<Bundle>>(HttpStatus.NOT_FOUND);
-	  
-	  return response;
-  }
-  private Sort sortByPriceDesc() {
-	 Sort sort = Sort.by(Sort.Order.desc("price"));
-	 return sort;
-  }
 }
